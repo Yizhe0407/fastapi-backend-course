34c0e66bc988e422f8ce67c739c69ab164938780
yizhe <liaoyizhe75@gmail.com>
2024-10-24 15:56:56 +0800

    Increase post method

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from fastapi import fastapi
+from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    description: str = None
 
 @app.get('/')
 def read_root():
     return {"Hello world!"}
+
+@app.post('/items/')
+def create_item(item: Item):
+    print(f"Received item: {item}")
+    return {"message": "Item received", "item": item}
