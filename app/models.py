cd4687ada04d2995ff9d61f4e8e97a221b31a995
Fuyuan Cheng <gloomcheng@gmail.com>
2024-12-12 15:51:28 +0800

    Update users model

--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class Todo(Base):
 class User(Base):
     __tablename__ = "users"
 
-    id = Column()
-    username = Column()
-    password = Column()
-    email = Column()
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
